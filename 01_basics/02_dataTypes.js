07d07d9e26a30fd48e0240d5ac9a2eea1470ca92
UmakanthAntharam <154390258+UmakanthAntharam@users.noreply.github.com>
2025-05-21 06:52:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node js, not browser
+
+console.log( 3 
+    + 3 );  // code readability should be high 
+    
+let name = "Umakanth"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 to the power 53
+//bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
